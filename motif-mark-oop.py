7f279bad5d25bd6a33b3f4e5eb91d956873cbe2d
Catherine Carmosino <catcar@uoregon.edu>
2026-02-10 14:42:13 -0800

    adding my oneline fasta function

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -21,3 +21,38 @@ args=get_args()
 f:str = args.f
 #reassiging args.m filename as m
 m:str = args.m
+
+
+def oneline_fasta(input_file:str, output_file:str):
+    '''takes a FASTA file with multiple sequence lines per record
+    and converts each record into 2 lines (header and one sequence)'''
+    #writting a new file named the output file
+    with open(output_file, "w") as f:
+        #opening the reading FASTA file!
+        with open(input_file, "r") as fh:
+            #initalizing an empty string to hold the sequences
+            seq_line = ''
+            #reading through the lines in the file
+            for line in fh:
+                #runs only when at a header
+                if ">" in line:
+                    #this is for the first line case when sequence is
+                    #empty but I need the first header
+                    if seq_line == '':
+                        #writting the first header
+                        f.write(line)
+                    #this is for when we reach the next header
+                    else:
+                        #writting the sequence!
+                        f.write(f"{seq_line}\n")
+                        #emptying the sequence holder
+                        seq_line = ''
+                        #writting the next header
+                        f.write(line)
+                #runs when at sequence line
+                else:
+                    #adds the line to the string to make sequence one line
+                    line = line.strip("\n")
+                    seq_line += line
+            #the last case to write the last sequence
+            f.write(seq_line)
