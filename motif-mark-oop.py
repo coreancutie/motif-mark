321e47477295f41017c0033f076d7338e652fbf3
Catherine Carmosino <catcar@uoregon.edu>
2026-02-10 14:39:05 -0800

    adding argparse statements

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -2,3 +2,22 @@
 
 #adding imports
 import cairo
+import argparse
+
+#adding argparse statements
+def get_args():
+    '''these are the arguments that will be parsed in for this python script'''
+    parser = argparse.ArgumentParser(description="Getting the input and output arguments for the motif-mark script")
+    
+    parser.add_argument("-f", help="The absolute path to the input FASTA file", required=True)
+    parser.add_argument("-m", help="The absolute path to the input motif file (a txt file with one motif on each line)", required=True)
+    
+    return parser.parse_args()
+
+#calling the get_args function to get the arguments
+args=get_args()
+
+#reassiging args.f filename as f
+f:str = args.f
+#reassiging args.m filename as m
+m:str = args.m
